17725c0767b762585ed91a3e56ce32318ac52f23
strbtrfll <strbtrfll@gmail.com>
2024-03-06 14:30:02 +0200

    Свайп
    +Організація, та нові спрайти

--- Tiles/Field/Field_tile.gd
+++ Tiles/Field/Field_tile.gd
@@ -5,7 +5,7 @@ func _on_button_pressed():
 	get_tree().call_group("TileSheet","show_pink_tile_info")
 
 func disable_button():
-	$PinkButton.disabled = true
+	$FieldButton.disabled = true
 
 func inable_button():
-	$PinkButton.disabled = false
+	$FieldButton.disabled = false
--- Tiles/Rock/Rock_tile.gd
+++ Tiles/Rock/Rock_tile.gd
@@ -5,7 +5,7 @@ func _on_orange_button_pressed():
 	get_tree().call_group("TileSheet","show_oragne_tile_info")
 
 func disable_button():
-	$OrangeButton.disabled = true
+	$RockButton.disabled = true
 
 func inable_button():
-	$OrangeButton.disabled = false
+	$RockButton.disabled = false
